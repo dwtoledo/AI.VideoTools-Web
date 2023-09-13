dec1b14531e6c48d68f097d9be79823bf885ca6c
Douglas Toledo <dwtoledo@outlook.com>
2023-09-13 10:06:04 -0400

    Implement Initial Header

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,9 +1,25 @@
 import { Button } from '@/components/ui/button'
+import { Github } from 'lucide-react'
+import { Separator } from './components/ui/separator'
 
 export function App() {
   return (
-    <div>
-      <Button>Teste</Button>
+    <div className="min-h-screen flex flex-col">
+      <header className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Developed by @dwtoledo on Rocketseat NLW
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            GitHub
+          </Button>
+        </div>
+      </header>
+      <main className="flex-1"></main>
     </div>
   )
 }
