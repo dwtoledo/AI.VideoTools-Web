2a158ab2271cf562d900cb824a53b8f1945daef3
Douglas Toledo <dwtoledo@outlook.com>
2023-09-18 10:05:14 -0400

    Implement User Alert Dialog on AI Prompt Select

--- upload-ai-web/src/components/ai-prompt-select.tsx
+++ upload-ai-web/src/components/ai-prompt-select.tsx
@@ -8,6 +8,7 @@ import {
 } from './ui/select'
 import { api } from '@/lib/axios'
 import { Label } from './ui/label'
+import { UserAlert } from './user-alert'
 
 export interface Prompt {
   id: string
@@ -19,19 +20,48 @@ interface AIPromptSelectionProps {
   onAIPromptSelected: (Prompt: Prompt) => void
 }
 
+let userAlertTitle: string
+let userAlertMessage: string
+let userAlertActionText: string
+
 export function AIPromptSelect(props: AIPromptSelectionProps) {
+  const [openUserAlert, setOpenUserAlert] = useState<boolean>(false)
   const [prompts, setPrompts] = useState<Array<Prompt> | null>(null)
 
   useEffect(() => {
-    api.get('/prompts').then((response) => {
-      setPrompts(response.data)
-    })
+    api
+      .get('/prompts')
+      .then((response) => {
+        setPrompts(response.data)
+      })
+      .catch((error) => {
+        handleOpenUserAlert(
+          'Error!',
+          `It is not possible to load the prompt selection. Please contact the system administrator: ${error.message}`,
+          'Ok',
+        )
+      })
   }, [])
 
   function handleAIPromptSelected(promptId: string) {
     const selectedAIPrompt = prompts?.find((prompt) => prompt.id === promptId)
     if (!selectedAIPrompt) return
     props.onAIPromptSelected(selectedAIPrompt)
+  }
+
+  function handleUserAlertClose() {
+    setOpenUserAlert(false)
+  }
+
+  function handleOpenUserAlert(
+    title: string,
+    message: string,
+    actionText: string,
+  ) {
+    userAlertTitle = title
+    userAlertMessage = message
+    userAlertActionText = actionText
+    setOpenUserAlert(true)
   }
 
   return (
@@ -51,6 +81,13 @@ export function AIPromptSelect(props: AIPromptSelectionProps) {
           })}
         </SelectContent>
       </Select>
+      <UserAlert
+        onClose={handleUserAlertClose}
+        openTrigger={openUserAlert}
+        title={userAlertTitle}
+        message={userAlertMessage}
+        actionText={userAlertActionText}
+      />
     </>
   )
 }
